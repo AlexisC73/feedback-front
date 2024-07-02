d72f1e23d565007db4f8c87f0e4af4d32a3e6a43
Alexis Comte <alexis.comte73@icloud.com>
2024-07-02 13:36:27 +0200

    update add feedback page

--- src/components/SortFilter/SortFilterItem/SortFilterItem.tsx
+++ src/components/SortFilter/SortFilterItem/SortFilterItem.tsx
@@ -1,6 +1,6 @@
 import { CheckIcon } from "../../../assets/icons";
 
-export function SortFilterItem ({filter, active}: {filter: string, active: boolean}) {
+export function SortFilterItem ({filter, active}: {filter: string, active?: boolean}) {
   return (
     <li className="px-6 py-3 text-#647196 hover:text-#AD1FEA cursor-pointer bg-white flex justify-between items-center">{filter} {active && <CheckIcon className="text-2.5" />}</li>
   )
--- src/pages/AddFeedback.tsx
+++ src/pages/AddFeedback.tsx
@@ -1,12 +1,48 @@
 import { Link } from "react-router-dom";
 import { GoBackButton } from "../components/BoBackButton/GoBackButton";
 import Layout from "../Layout";
+import { InputHeader } from "../components/form/input-header/InputHeader";
+import { Input } from "../components/form/input/Input";
+import { FormGroup } from "../components/form/form-group/FormGroup";
+import { Dropdown } from "../components/form/dropdown/Dropdown";
+import { useState } from "react";
 
 export function AddFeedbackPage () {
+  const options = ["Feature", "UI", "UX", "Enhancement", "Bug"]
+  const status = ["Suggestion", "Planned", "In-Progress", "Live"]
+
+  const [currentOption, setCurrentOption] = useState<string>(options[0])
+  const [currentStatus, setCurrentStatus] = useState<string>(status[0])
+
+  const handleSelectOption = (selected: string) => {
+    setCurrentOption(selected)
+  }
+
+  const handleUpdateStatus = (status: string) => {
+    setCurrentStatus(status)
+  }
+
   return (
     <Layout.emptyLayout>
-      <div className="px-6 py-8 flex">
-        <Link to="/" ><GoBackButton /></Link>
+      <div className="px-6 py-8.5 flex flex-col gap-y-8.5 items-start">
+        <div>
+          <Link to="/" ><GoBackButton /></Link>
+        </div>
+        <div className="bg-white p-6 pt-11 w-full rounded-2.5 flex flex-col gap-y-6">
+          <p className="text-4.5 font-bold text-#3A4374 line-height-6.5 mb-6">Create New Feedback</p>
+          <FormGroup>
+            <InputHeader htmlFor="title" label="Feedback Title" description="Add a short, descriptive headline" />
+            <Input error="Can't be empty" />
+          </FormGroup>
+          <FormGroup>
+            <InputHeader htmlFor="category" label="Category" description="Choose a category for your feedback" />
+            <Dropdown onSelect={handleSelectOption} current={currentOption} options={options} />
+          </FormGroup>
+          <FormGroup>
+            <InputHeader htmlFor="description" label="Feedback Detail" description="Include any specific comments on what should be improved, added, etc." />
+            
+          </FormGroup>
+        </div>
       </div>
     </Layout.emptyLayout>
   )
