d6957437533831b340e40b3f7469202b968ce2a7
Alexis Comte <alexis.comte73@icloud.com>
2024-07-02 14:14:59 +0200

    adding responsive for feedback form

--- src/pages/UpdateFeedback.tsx
+++ src/pages/UpdateFeedback.tsx
@@ -8,25 +8,32 @@ import { Dropdown } from "../components/form/dropdown/Dropdown";
 import { useState } from "react";
 import { Textarea } from "../components/form/textarea/Textarea";
 import { Button } from "../components/Button/button";
+import { PenIcon } from "../assets/icons";
 
-export function AddFeedbackPage () {
+export function UpdateFeedbackPage () {
   const options = ["Feature", "UI", "UX", "Enhancement", "Bug"]
+  const status = ["Suggestion", "Planned", "In-Progress", "Live"]
 
   const [currentOption, setCurrentOption] = useState<string>(options[0])
+  const [currentStatus, setCurrentStatus] = useState<string>(status[0])
 
   const handleSelectOption = (selected: string) => {
     setCurrentOption(selected)
   }
 
+  const handleUpdateStatus = (status: string) => {
+    setCurrentStatus(status)
+  }
+
   return (
     <Layout.emptyLayout>
-      <div className="px-6 py-8.5 flex flex-col gap-y-13.75 items-start md:max-w-135 md:mx-auto md:px-0 md:my-auto">
+      <div className="px-6 py-8.5 flex flex-col gap-y-13.75 items-start md:max-w-135 md:mx-auto md:px-0 md:py-14">
         <div>
           <Link to="/" ><GoBackButton /></Link>
         </div>
         <div className="bg-white p-6 pt-11 w-full rounded-2.5 flex flex-col gap-y-6 relative">
-          <div className="bg-custom-radial h-10 w-10 md:w-14 md:h-14 rounded-full flex items-center justify-center text-white absolute -top-5 md:-top-7 text-5 md:text-6 font-bold">+</div>
-          <p className="text-4.5 font-bold text-#3A4374 line-height-6.5 mb-6">Create New Feedback</p>
+          <div className="bg-custom-radial h-10 w-10 md:h-14 md:w-14 rounded-full flex items-center justify-center text-white absolute -top-7 md:-top-7 text-5 md:text-6 font-bold"><PenIcon /></div>
+          <p className="text-4.5 font-bold text-#3A4374 line-height-6.5 mb-6">Editing ‘Add a dark theme option’</p>
           <FormGroup>
             <InputHeader htmlFor="title" label="Feedback Title" description="Add a short, descriptive headline" />
             <Input error="Can't be empty" />
@@ -36,15 +43,20 @@ export function AddFeedbackPage () {
             <Dropdown onSelect={handleSelectOption} current={currentOption} options={options} />
           </FormGroup>
           <FormGroup>
+            <InputHeader htmlFor="status" label="Update Status" description="Change feature state" />
+            <Dropdown onSelect={handleUpdateStatus} current={currentStatus} options={status} />
+          </FormGroup>
+          <FormGroup>
             <InputHeader htmlFor="description" label="Feedback Detail" description="Include any specific comments on what should be improved, added, etc." />
             <Textarea error="Can't be empty" />
           </FormGroup>
-          <div className="mt-10 flex flex-col gap-y-4 md:flex-row md:justify-end md:gap-x-4">
-            <div className="w-full md:w-36 md:order-last">
-              <Button fullWidth>Add Feedback</Button>
+          <div className="mt-10 flex flex-col gap-y-4 md:flex-row md:justify-between">
+            <div className="md:w-23.25">
+              <Button fullWidth type="danger">Delete</Button>
             </div>
-            <div className="w-full md:w-23.25">
-              <Button fullWidth type="tertiary">Cancel</Button>
+            <div className="order-first flex flex-col gap-y-4 md:order-last md:flex-row md:gap-x-4">
+              <div className="md:w-36 md:order-last"><Button fullWidth>Save Changes</Button></div>
+              <div className="md:w-23.25"><Button fullWidth type="tertiary">Cancel</Button></div>
             </div>
           </div>
         </div>
--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom"
 import { HomePage } from "./pages/Home"
 import { FeedbackDetailsPage } from "./pages/FeedbackDetails"
 import { AddFeedbackPage } from "./pages/AddFeedback"
+import { UpdateFeedbackPage } from "./pages/UpdateFeedback"
 
 export const createRouter = () => createBrowserRouter([
     {
@@ -15,6 +16,10 @@ export const createRouter = () => createBrowserRouter([
         {
           path: "/feedbacks/new",
           element: <AddFeedbackPage />
+        },
+        {
+          path: "/feedbacks/edit/:id",
+          element: <UpdateFeedbackPage />
         },
         {
           path: "/feedbacks/:id",
