80693c5a7629546a20ee38082a6049469e586a90
Alexis Comte <alexis.comte73@icloud.com>
2024-07-02 14:29:57 +0200

    update the dropdown for the filter in suggestion

--- src/components/SelectSortFilter/SelectSortFilter.tsx
+++ src/components/SelectSortFilter/SelectSortFilter.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react"
-import { SortFilter } from "../SortFilter/SortFilter"
 import { ArrowIcon } from "../../assets/icons"
+import { DropdownMenu } from "../form/dropdown/Dropdown"
 
-export function SelectSortFilter ({activeFilter}: {activeFilter: string}) {
+export function SelectSortFilter ({activeFilter, filters, onUpdateFilter}: {activeFilter: string, filters: string[], onUpdateFilter: (filter: string) => void}) {
   const [isFilterOpen, setIsFilterOpen] = useState(false)
 
   const toggleFilter = () => {
@@ -13,8 +13,8 @@ export function SelectSortFilter ({activeFilter}: {activeFilter: string}) {
     <div className="text-white relative">
       <p onClick={toggleFilter} className="flex items-center gap-x-2 text-#F2F4FE text-opacity-75 text-3.25 md:text-3.5 cursor-pointer">Sort by : <strong>{activeFilter}</strong> <ArrowIcon className={`text-1.75 text-white ${isFilterOpen ? "rotate-0" : "rotate-180"}`} /></p>
       { isFilterOpen && (
-        <div className="absolute left-0 top-10.5">
-          <SortFilter activeFilter={activeFilter} />
+        <div className="absolute left-0 top-10.5 w-63.75 bg-white rounded-2.5">
+          <DropdownMenu options={filters} current={activeFilter} onSelect={onUpdateFilter} />
         </div>)
       }
     </div>
--- src/components/Suggestions/Suggestions.tsx
+++ src/components/Suggestions/Suggestions.tsx
@@ -2,13 +2,21 @@ import { Link } from "react-router-dom"
 import { SuggestionsIcon } from "../../assets/icons"
 import { Button } from "../Button/button"
 import { SelectSortFilter } from "../SelectSortFilter/SelectSortFilter"
+import { useState } from "react"
 
 export function Suggestions () {
+  const filters = ["Most Upvotes", "Least Upvotes", "Most Comments", "Least Comments"]
+  const [currentFilter, setCurrentFilter] = useState("Most Upvotes")
+
+  const handleUpdateFilter = (filter: string) => {
+    setCurrentFilter(filter)
+  }
+
   return (
     <div className="flex bg-#373F68 p-6 md:pr-3 lg:pr-4 py-3.5 md:rounded-2.5 justify-between">
       <div className="flex gap-x-9.5 items-center">
         <p className="hidden text-white gap-x-4 items-center md:flex"><SuggestionsIcon className="text-6 text-white" /> <strong>6 Suggestions</strong></p>
-        <SelectSortFilter activeFilter="Most Upvotes" />
+        <SelectSortFilter activeFilter={currentFilter} filters={filters} onUpdateFilter={handleUpdateFilter} />
       </div>
       <Link to="/feedbacks/new" className="w-33.5 md:w-39.5">
         <Button>+ Add Feedback</Button>
