10b22699cd11703b2b3ae2e3e54b8c09a99558c0
Alexis Comte <alexis.comte73@icloud.com>
2024-07-02 13:35:45 +0200

    adding form input and label

---
+++ src/components/form/input-header/InputHeader.tsx
@@ -0,0 +1,8 @@
+export function InputHeader ({htmlFor, label, description}: {htmlFor: string, label: string, description: string}) {
+  return (
+    <div id="input-header" className="flex flex-col gap-y-0.5">
+      <label htmlFor={htmlFor} className="text-#3A4374 text-3.25 line-height-4.75 font-bold -tracking-0.18px">{label}</label>
+      <p className="text-3.25 line-height-4.75 text-#647196">{description}</p>
+    </div>
+  )
+}
---
+++ src/components/form/input/Input.tsx
@@ -0,0 +1,9 @@
+export function Input ({error}: {error?: string}) {
+
+  return (
+    <div className="flex flex-col gap-y-1 relative">
+      <input className={`bg-#F7F8FD rounded-1.25 h-12 outline-none px-6 text-4 md:text-3.75 focus:border focus:border-1 focus:border-#4661E6 ${!error ? "" : "border border-1 border-#D73737"}`} />
+      <span id="error" className="text-3.5 text-#D73737 absolute top-12.25">{error}</span>
+    </div>
+  )
+}
