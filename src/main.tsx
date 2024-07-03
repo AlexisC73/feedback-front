dc613c31c7832dc317a4baa60095fb3096add9e1
Alexis Comte <alexis.comte73@icloud.com>
2024-07-03 15:14:20 +0200

    adding provider redux

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,16 @@ import { createRouter } from './router.tsx'
 import { RouterProvider } from 'react-router-dom'
 import "@/index.css"
 import '@unocss/reset/tailwind.css'
+import { Provider } from 'react-redux'
+import { createStore } from './store/store.ts'
 
 const router = createRouter()
+const store = createStore({})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
