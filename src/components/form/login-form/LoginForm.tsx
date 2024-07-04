68ebabcecb65102ebed030b1b0d2a4bbde976a35
Alexis Comte <alexis.comte73@icloud.com>
2024-07-04 15:40:42 +0200

    adding error handling for login form

--- src/components/form/login-form/LoginForm.tsx
+++ src/components/form/login-form/LoginForm.tsx
@@ -6,9 +6,10 @@ import { Link } from "react-router-dom";
 
 export interface LoginFormProps {
   loginFn: (props: {email: string, password: string}) => Promise<void>
+  fieldsError: {[key: string]: string}
 }
 
-export function LoginForm ({ loginFn }: LoginFormProps) {
+export function LoginForm ({ loginFn, fieldsError }: LoginFormProps) {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -18,17 +19,18 @@ export function LoginForm ({ loginFn }: LoginFormProps) {
     const password = formData.get("password")?.toString() ?? ""
     loginFn({ email, password})
   }
+
   return (
     <form onSubmit={handleSubmit} className="bg-white p-6 pt-11 w-full rounded-2.5 flex flex-col gap-y-6 relative">
           <div className="bg-custom-radial h-10 w-10 md:w-14 md:h-14 rounded-full flex items-center justify-center text-white absolute -top-5 md:-top-7 text-5 md:text-6 font-bold">+</div>
           <p className="text-4.5 font-bold text-#3A4374 line-height-6.5 mb-6">Sign In</p>
           <FormGroup>
             <InputHeader htmlFor="email" label="Mail address" />
-            <Input name="email" />
+            <Input name="email" error={fieldsError.email} />
           </FormGroup>
           <FormGroup>
             <InputHeader htmlFor="password" label="Password" />
-            <Input name="password" />
+            <Input name="password" error={fieldsError.password} />
           </FormGroup>
           <div className="mt-10 flex flex-col gap-y-4 md:flex-row md:justify-end md:gap-x-4">
             <button type="submit" className="w-full md:order-last">
--- src/store/auth/app/login/LoginComponent.tsx
+++ src/store/auth/app/login/LoginComponent.tsx
@@ -1,13 +1,28 @@
 import { LoginForm } from "@/components/form/login-form/LoginForm";
 import { useAppDispatch } from "@/store/store-hooks";
-import { loginThunk } from "../../usecases/login.usecase";
+import { loginThunk, LoginThunkResultType } from "../../usecases/login.usecase";
+import { useState } from "react";
 
 export function LoginComponent () {
+  const [fieldsError, setFieldsError] = useState<{[key: string]: string}>({})
   const dispatch = useAppDispatch()
   const performLogin = async ({email, password}: {email: string, password: string}) => {
-    dispatch(loginThunk({email, password}))
+    setFieldsError({})
+    dispatch(loginThunk({email, password})).then((res) => {
+      if(res.meta.requestStatus === "fulfilled") {
+        console.log(res.payload)
+      } else {
+        if(res.payload?.type === LoginThunkResultType.FIELD_ERROR) {
+          const errors: {[key: string]: string} = {}
+          res.payload.errors.forEach((errorField) => {
+            errors[errorField.field] = errorField.errors[0]
+          })
+          setFieldsError(errors)
+        }
+      }
+    })
   }
   return (
-    <LoginForm loginFn={performLogin} />
+    <LoginForm fieldsError={fieldsError} loginFn={performLogin} />
   )
 }
