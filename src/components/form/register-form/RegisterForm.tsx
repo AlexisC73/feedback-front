499d48b98c41d0899c5af30daade7bbc8ab7288d
Alexis Comte <alexis.comte73@icloud.com>
2024-07-04 14:31:13 +0200

    adding error handling for the register usecase

--- src/components/form/register-form/RegisterForm.tsx
+++ src/components/form/register-form/RegisterForm.tsx
@@ -3,21 +3,33 @@ import { FormGroup } from "../form-group/FormGroup";
 import { InputHeader } from "../input-header/InputHeader";
 import { Input } from "../input/Input";
 import { Button } from "@/components/Button/button";
+import { useState } from "react";
 
 interface RegisterFormProps {
   registerFn: (props: {email: string, password: string}) => Promise<void>
+  fieldsError: {[key: string]: string}
 }
 
-export function RegisterForm ({registerFn}: RegisterFormProps) {
+export function RegisterForm ({registerFn, fieldsError}: RegisterFormProps) {
+  const [errors, setErrors] = useState<{[key: string]: string | undefined}>({
+    email: fieldsError.email,
+    password: fieldsError.password,
+  })
+
+  const resetErrors = () => {
+    setErrors({})
+  }
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    resetErrors()
     const form = e.currentTarget
     const formData = new FormData(form)
     const email = formData.get("email")?.toString() ?? ""
     const password = formData.get("password")?.toString() ?? ""
     const confirmationPassword = formData.get("confirmation-password")?.toString() ?? ""
     if(password !== confirmationPassword) {
-      alert("Passwords do not match")
+      setErrors(prev => ({...prev, confirmation: "Passwords do not match"}))
       return
     }
     registerFn({ email, password})
@@ -28,15 +40,15 @@ export function RegisterForm ({registerFn}: RegisterFormProps) {
       <p className="text-4.5 font-bold text-#3A4374 line-height-6.5 mb-6">Create your account</p>
       <FormGroup>
         <InputHeader htmlFor="email" label="Mail address" />
-        <Input name="email" />
+        <Input name="email" error={fieldsError.email ?? errors.email} />
       </FormGroup>
       <FormGroup>
         <InputHeader htmlFor="password" label="Password" />
-        <Input name="password" />
+        <Input name="password" error={fieldsError.password ?? errors.password} />
       </FormGroup>
       <FormGroup>
         <InputHeader htmlFor="confirmation-password" label="Verification password" description="Re-type your password" />
-        <Input name="confirmation-password" />
+        <Input name="confirmation-password" error={errors.confirmation} />
       </FormGroup>
       <div className="mt-10 flex flex-col gap-y-4 md:flex-row md:justify-end md:gap-x-4">
         <button type="submit" className="w-full md:order-last">
--- src/store/auth/app/register/RegisterComponent.tsx
+++ src/store/auth/app/register/RegisterComponent.tsx
@@ -1,20 +1,32 @@
 import { RegisterForm } from "@/components/form/register-form/RegisterForm";
 import { useAppDispatch } from "@/store/store-hooks";
 import { registerThunk, RegisterThunkResultType } from "../../usecases/register.usecase";
-import { unwrapResult } from "@reduxjs/toolkit";
+import { useState } from "react";
 
 export function RegisterComponent () {
   const dispatch = useAppDispatch()
 
+  const [errors, setErrors] = useState<{[key: string]: string}>({})
+
   const performRegister = async ({email, password}: {email: string, password: string}) => {
-    dispatch(registerThunk({email, password})).then(unwrapResult).then(res => {
-      if(res.type === RegisterThunkResultType.SUCCESS) {
-        alert("User registered with email " + email)
+    setErrors({})
+    dispatch(registerThunk({email, password})).then(res => {
+      if(res.meta.requestStatus === "fulfilled") {
+        if(res.payload?.type === RegisterThunkResultType.SUCCESS) {
+          console.log("Success") // TODO: redirect to login page and show a success message as a toast ?
+        }
+      } else {
+        if(res.payload?.type === RegisterThunkResultType.FIELD_ERROR) {
+          const errors: {[key: string]: string} = {}
+          console.log(res.payload.errors)
+          res.payload.errors.forEach(fieldError => errors[fieldError.field] = fieldError.errors[0])
+          setErrors(errors)
+         }
       }
     })
   }
 
   return (
-    <RegisterForm registerFn={performRegister} />
+    <RegisterForm registerFn={performRegister} fieldsError={errors} />
   )
 }
