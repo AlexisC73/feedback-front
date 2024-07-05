4aa69998856ba3eda3228ff9fa8614bfe0f70403
Alexis Comte <alexis.comte73@icloud.com>
2024-07-05 16:41:34 +0200

    update access role helper

--- src/helpers/roleAccess.ts
+++ src/helpers/roleAccess.ts
@@ -1,13 +1,13 @@
 import { Role } from "@/store/account/models/account"
 
-export function isAdmin({role, strict}: {role: Role, strict: boolean}) {
+export function isAdmin({role, strict = false}: {role: Role, strict?: boolean}) {
   return strict ? role === Role.ADMIN : role >= Role.ADMIN
 }
 
-export function isModerator({role, strict}: {role: Role, strict: boolean}) {
+export function isModerator({role, strict = false}: {role: Role, strict?: boolean}) {
   return strict ? role >= Role.MODERATOR && role < Role.ADMIN : role >= Role.MODERATOR 
 }
 
-export function isUser({role, strict}: {role: Role, strict: boolean}) {
+export function isUser({role, strict = false}: {role: Role, strict?: boolean}) {
   return strict ? role >= Role.USER && role < Role.MODERATOR : role >= Role.USER
 }
