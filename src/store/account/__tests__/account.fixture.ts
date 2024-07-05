929664544f291e5d73f110d1d0824e47c003eaf3
Alexis Comte <alexis.comte73@icloud.com>
2024-07-05 13:59:17 +0200

    using new state builder for test

--- src/store/account/__tests__/account.fixture.ts
+++ src/store/account/__tests__/account.fixture.ts
@@ -1,43 +1,45 @@
 import { registerThunk, RegisterUsecaseParams } from "@/store/auth/usecases/register.usecase"
-import { InMemoryAccountRepository } from "../infra/in-memory-account.repository"
 import { expect } from "vitest"
-import { AppStore } from "@/store/store"
 import { EmailVO } from "@/store/value-objects/email"
 import { AuthState } from "@/store/auth/auth-reducer"
 import { loginThunk, LoginUsecaseParams } from "@/store/auth/usecases/login.usecase"
 import { DomainAccount } from "../models/account"
+import { StateBuilder } from "@/store/state-builder"
 
-export const createAccountFixture = (store: AppStore, { accountRepository }: { accountRepository: InMemoryAccountRepository }) => {
+export const createAccountFixture = ( stateBuilder: StateBuilder) => {
 
   let resultType: string | undefined
 
   return {
     givenNoAccountExists() {
-      accountRepository.accounts = []
+      stateBuilder.getAccountRepository().accounts = []
+    },
+    givenIsAuthenticatedAs(authAccount: AuthState["account"]) {
+      stateBuilder.setStore({...stateBuilder.getStore().getState(), auth: {account: authAccount, loading: false}})
     },
     givenAccountExists(accounts: DomainAccount[]) {
-      accountRepository.accounts = accounts
+      stateBuilder.getAccountRepository().accounts = accounts
     },
     async whenAccountRegister(registerPayload: RegisterUsecaseParams) {
-        const result = await store.dispatch(registerThunk(registerPayload))
+        const result = await stateBuilder.getStore().dispatch(registerThunk(registerPayload))
         resultType = result.payload?.type
     },
     async whenUserLogin(loginParam: LoginUsecaseParams) {
-      await store.dispatch(loginThunk(loginParam))
+      await stateBuilder.getStore().dispatch(loginThunk(loginParam))
     },
     thenResultTypeShouldBe(expectedType: string) {
       expect(resultType).toBe(expectedType)
     },
     thenAccountShouldExist(email: EmailVO) {
-      const repoAccount = accountRepository.accounts.find(a => a.email === email.value)
+      const repoAccount = stateBuilder.getAccountRepository().accounts.find(a => a.email === email.value)
       expect(repoAccount?.email).toBe(email.value)
     },
     thenAccountShouldNotExist(email: EmailVO) {
-      const repoAccount = accountRepository.accounts.find(a => a.email === email.value)
+      const repoAccount = stateBuilder.getAccountRepository().accounts.find(a => a.email === email.value)
       expect(repoAccount).toBeUndefined()
     },
     thenAuthStateShouldBe(expectedAuthState: AuthState) {
-      expect(store.getState().auth).toMatchObject(expectedAuthState)
+      expect(stateBuilder.getStore().getState().auth).toMatchObject(expectedAuthState)
     }
   }
 }
--- src/store/feedbacks/__tests__/feedback.fixture.ts
+++ src/store/feedbacks/__tests__/feedback.fixture.ts
@@ -1,23 +1,34 @@
-import { AppStore } from "@/store/store"
 import { FeedbackState } from "../feedback.reducer"
 import { expect } from "vitest"
 import { getFeedbacksThunk } from "../usecases/get-feedbacks.usecase"
-import { InMemoryFeedbackRepository } from "../infra/in-memory-feedback.repository"
-import { DomainFeedback } from "../models/feedback"
+import { addFeedbackThunk, AddFeedbackUsecaseParams } from "../usecases/add-feedback.usecase"
+import { StateBuilder } from "@/store/state-builder"
+import { Feedback } from "../models/feedback"
 
-export const createFeedbackFixture = (store: AppStore, { feedbackRepository }: {feedbackRepository: InMemoryFeedbackRepository}) => {
+export const createFeedbackFixture = (stateBuilder: StateBuilder) => {
   return {
     givenNoFeedbacksExists() {
-      feedbackRepository.feedbacks = []
+      stateBuilder.getFeedbackRepository().feedbacks = []
     },
-    givenFeedbacksExists(feedbacks: DomainFeedback[]) {
-      feedbackRepository.feedbacks = feedbacks
+    givenNextGeneratedId(id: string) {
+      stateBuilder.getIdProvider().id = id
+    },
+    givenFeedbacksExists(feedbacks: Feedback[]) {
+      stateBuilder.setStore({...stateBuilder.getStore().getState(), feedback: {data: [...feedbacks], loading: false}})
+      stateBuilder.getFeedbackRepository().feedbacks = [...feedbacks]
     },
     async whenGetFeedbacks() {
-      await store.dispatch(getFeedbacksThunk())
+      await stateBuilder.getStore().dispatch(getFeedbacksThunk())
+    },
+    async whenAddFeedback(feedback: AddFeedbackUsecaseParams) {
+      await stateBuilder.getStore().dispatch(addFeedbackThunk(feedback))
     },
     thenFeedbacksStateShouldBe(feedbackState: FeedbackState) {
-      expect(store.getState().feedback).toEqual(feedbackState)
+      expect(stateBuilder.getStore().getState().feedback).toMatchObject(feedbackState)
+    },
+    thenFeedbackShouldExists(feedback: Feedback) {
+      const fundFeedback = stateBuilder.getFeedbackRepository().feedbacks.find(f => f.id === feedback.id)
+      expect(fundFeedback).toMatchObject(feedback)
     }
   }
 }
