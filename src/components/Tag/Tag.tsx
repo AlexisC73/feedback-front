3d4dfd8e0f106028cb3fa1b5847bfb1bfd5f2006
Alexis Comte <alexis.comte73@icloud.com>
2024-07-05 16:03:57 +0200

    adding uppercase for tag if less or equal than 2 letters

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -11,7 +11,9 @@ export function Tag ({title, active, hoverable}: {title: "all" | "ui" | "ux" | "
   const activeStyle = "bg-#4661E6 text-white"
   const unactiveStyle = "bg-#F2F4FF text-#4661E6"
 
+  const tagTitle = title.length <= 2 ? title.toUpperCase() : title.charAt(0).toUpperCase() + title.slice(1)
+
   return (
-    <p className={`h-7.5 flex items-center rounded-2.5 font-semibold text-3.25 line-height-4.75 ${hoverable ? "hover:bg-#CFD7FF cursor-pointer" : ""} ${filtersStyle[title]} ${active ? activeStyle : unactiveStyle}`}>{title.charAt(0).toUpperCase() + title.slice(1)}</p>
+    <p className={`h-7.5 flex items-center rounded-2.5 font-semibold text-3.25 line-height-4.75 ${hoverable ? "hover:bg-#CFD7FF cursor-pointer" : ""} ${filtersStyle[title]} ${active ? activeStyle : unactiveStyle}`}>{tagTitle}</p>
   )
 }
