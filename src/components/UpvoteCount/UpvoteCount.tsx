3c4f796215e550ca4ba96430b3e62a0ef9a3ba18
Alexis Comte <alexis.comte73@icloud.com>
2024-07-05 16:04:31 +0200

    fix error

--- src/components/UpvoteCount/UpvoteCount.tsx
+++ src/components/UpvoteCount/UpvoteCount.tsx
@@ -1,7 +1,6 @@
 import { ArrowIcon } from "@/assets/icons";
 
 export function UpvoteCount ({count, upvoted}: {count: number, upvoted: boolean}) {
-  console.log(upvoted)
   return (
     <p className={`hover:bg-#CFD7FF cursor-pointer flex items-center gap-x-2.5 pr-3.75 pt-1.5 pb-1.75 pl-4 rounded-2.5 md:p-0 md:flex-col md:pt-3.5 md:w-10 md:h-13.25 md:gap-y-2 ${upvoted ? "bg-#4661E6 text-white" : "bg-#F2F4FE text-#3A4374"}`}>
       <ArrowIcon className={`text-2 ${upvoted ? "text-white" : "text-#4661E6"}`} />
