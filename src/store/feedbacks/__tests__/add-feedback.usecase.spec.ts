cccdc091c9c8a0a879efc245a617665598c395c9
Alexis Comte <alexis.comte73@icloud.com>
2024-07-05 15:36:12 +0200

    update test for add feedback usecase

--- src/store/feedbacks/__tests__/add-feedback.usecase.spec.ts
+++ src/store/feedbacks/__tests__/add-feedback.usecase.spec.ts
@@ -16,7 +16,58 @@ describe("Add Feedback Usecase", () => {
   })
 
   test("Should add a feedback with suggestion status", async () => {
-    const feedback = feedbackBuilder().withStatus(FeedbackStatus.SUGGESTION).build()
+    const defaultFeedback = feedbackBuilder()
+    const newFeedback = defaultFeedback.build()
+    const expectedFeedback = defaultFeedback.withStatus(FeedbackStatus.SUGGESTION).build()
+
+    feedbackFixture.givenNoFeedbacksExists()
+    feedbackFixture.givenNextGeneratedId(newFeedback.id)
+    accountFixture.givenIsAuthenticatedAs({
+      email: "test@email.fr",
+      id: newFeedback.owner
+    })
+
+    await feedbackFixture.whenAddFeedback({
+      category: newFeedback.category,
+      description: newFeedback.description,
+      title: newFeedback.title
+    })
+
+    feedbackFixture.thenFeedbacksStateShouldBe({
+      data: [expectedFeedback],
+      loading: false
+    })
+    feedbackFixture.thenFeedbackShouldExists(expectedFeedback)
+  })
+
+  test("Given some Feedbacks already exists", async () => {
+    const defaultFeedback = feedbackBuilder().withId("4")
+    const newFeedback = defaultFeedback.build()
+    const expectedNewFeedback = defaultFeedback.withStatus(FeedbackStatus.SUGGESTION).build()
+    const existingFeedback = [feedbackBuilder().withId("2").build(), feedbackBuilder().withId("3").build()]
+
+    feedbackFixture.givenFeedbacksExists(existingFeedback)
+    feedbackFixture.givenNextGeneratedId(newFeedback.id)
+    accountFixture.givenIsAuthenticatedAs({
+      email: "test@email.fr",
+      id: newFeedback.owner
+    })
+
+    await feedbackFixture.whenAddFeedback({
+      category: newFeedback.category,
+      description: newFeedback.description,
+      title: newFeedback.title
+    })
+
+    feedbackFixture.thenFeedbacksStateShouldBe({
+      data: [...existingFeedback, expectedNewFeedback],
+      loading: false
+    })
+    feedbackFixture.thenFeedbackShouldExists(expectedNewFeedback)
+  })
+
+  test("When adding new feedback, should have 0 upvotes and comments", async () => {
+    const feedback = feedbackBuilder().build()
 
     feedbackFixture.givenNoFeedbacksExists()
     feedbackFixture.givenNextGeneratedId(feedback.id)
@@ -31,34 +82,6 @@ describe("Add Feedback Usecase", () => {
       title: feedback.title
     })
 
-    feedbackFixture.thenFeedbacksStateShouldBe({
-      data: [feedback],
-      loading: false
-    })
-    feedbackFixture.thenFeedbackShouldExists(feedback)
-  })
-
-  test("Given some Feedbacks already exists", async () => {
-    const feedback = feedbackBuilder().withId('4').withStatus(FeedbackStatus.SUGGESTION).build()
-    const existingFeedback = [feedbackBuilder().withId("2").build(), feedbackBuilder().withId("3").build()]
-
-    feedbackFixture.givenFeedbacksExists(existingFeedback)
-    feedbackFixture.givenNextGeneratedId(feedback.id)
-    accountFixture.givenIsAuthenticatedAs({
-      email: "test@email.fr",
-      id: feedback.owner
-    })
-
-    await feedbackFixture.whenAddFeedback({
-      category: feedback.category,
-      description: feedback.description,
-      title: feedback.title
-    })
-
-    feedbackFixture.thenFeedbacksStateShouldBe({
-      data: [...existingFeedback, feedback],
-      loading: false
-    })
-    feedbackFixture.thenFeedbackShouldExists(feedback)
+    feedbackFixture.thenFeedbackShouldExists(feedbackBuilder().withStatus(FeedbackStatus.SUGGESTION).withUpvotes(0).withComments(0).build())
   })
 })
