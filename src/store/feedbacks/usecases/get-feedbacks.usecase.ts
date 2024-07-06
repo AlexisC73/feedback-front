c175bc9687a0d5afdf50389d22b55c4913fd4a8e
Alexis Comte <alexis.comte73@icloud.com>
2024-07-06 16:19:09 +0200

    update return type for get feedbacks usecase

--- src/store/feedbacks/usecases/get-feedbacks.usecase.ts
+++ src/store/feedbacks/usecases/get-feedbacks.usecase.ts
@@ -1,6 +1,6 @@
 import { createAppAsyncThunk } from "@/store/create-app-thunk";
 import { exhaustiveGuard } from "@/store/@shared/utiles/exhaustive-guard";
-import { ApiResultType, UsecaseResult, UsecaseResultType } from "@/store/@shared/models/resultType";
+import { ApiResultType, UsecaseCredentialError, UsecaseResultType, UsecaseUnknownError } from "@/store/@shared/models/resultType";
 
 export const getFeedbacksThunk = createAppAsyncThunk.withTypes<{rejectValue: GetFeedbacksThunkResult}>()("feedbacks/getFeedbacks", async (_, {extra: { feedbackRepository }, rejectWithValue}) => {
   try {
@@ -9,15 +9,15 @@ export const getFeedbacksThunk = createAppAsyncThunk.withTypes<{rejectValue: Get
       case ApiResultType.SUCCESS:
         return {type: UsecaseResultType.SUCCESS, data: feedbacks.data}
       case ApiResultType.CREDENTIAL_ERROR:
-        return rejectWithValue({type: UsecaseResultType.CREDENTIAL_ERROR, data: feedbacks.data} as UsecaseResult<UsecaseResultType, string | undefined>)
+        return rejectWithValue({type: UsecaseResultType.CREDENTIAL_ERROR, data: feedbacks.data} )
       case ApiResultType.UNKNOWN_ERROR:
-        return rejectWithValue({type: UsecaseResultType.UNKNOWN_ERROR, data: feedbacks.data} as UsecaseResult<UsecaseResultType, undefined>)
+        return rejectWithValue({type: UsecaseResultType.UNKNOWN_ERROR, data: feedbacks.data})
       default:
         exhaustiveGuard(feedbacks)
     }
   } catch(e) {
-    return rejectWithValue({type: UsecaseResultType.UNKNOWN_ERROR, data: undefined} as UsecaseResult<UsecaseResultType, undefined>)
+    return rejectWithValue({type: UsecaseResultType.UNKNOWN_ERROR, data: undefined})
   }
 })
 
-type GetFeedbacksThunkResult = UsecaseResult<UsecaseResultType, unknown>
+type GetFeedbacksThunkResult = UsecaseCredentialError | UsecaseUnknownError
