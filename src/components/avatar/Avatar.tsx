0956ead9ac30a747e4741e3b4792fc910f974b4c
Alexis Comte <alexis.comte73@icloud.com>
2024-07-16 23:51:50 +0200

    update min-width for avatar

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -2,10 +2,10 @@
 export function Avatar ({ imageUrl, username }: {imageUrl: string | null, username: string}) {
   if(imageUrl) {
     return (
-      <img src={imageUrl} alt={"profile-picture"} className="rounded-full h-10 w-10" />
+      <img src={imageUrl} alt={"profile-picture"} className="rounded-full min-h-10 min-w-10 h-10 w-10" />
     )
   }
   return (
-    <div className="rounded-full flex items-center justify-center h-10 w-10 text-white font-bold text-6 bg-blue-6 bg-opacity-60">{username.charAt(0).toLocaleUpperCase()}</div>
+    <div className="rounded-full flex items-center justify-center min-h-10 min-w-10 h-10 w-10 text-white font-bold text-6 bg-blue-6 bg-opacity-60">{username.charAt(0).toLocaleUpperCase()}</div>
   )
 }
