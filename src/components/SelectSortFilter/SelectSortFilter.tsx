5d9d9924b2716d8a4ac0f05e9550e13c272f9b36
Alexis Comte <alexis.comte73@icloud.com>
2024-07-16 16:49:58 +0200

    fix language error on sort filter

--- src/components/SelectSortFilter/SelectSortFilter.tsx
+++ src/components/SelectSortFilter/SelectSortFilter.tsx
@@ -18,7 +18,7 @@ export function SelectSortFilter ({activeFilter, filters, onUpdateFilter}: {acti
 
   return (
     <div className="text-white relative">
-      <p onClick={toggleFilter} className="flex items-center gap-x-2 text-#F2F4FE text-opacity-75 text-3.25 md:text-3.5 cursor-pointer">Sort by : <strong>{t(`sort_filters.${activeFilter}`)}</strong> <ArrowIcon className={`text-1.75 text-white ${isFilterOpen ? "rotate-0" : "rotate-180"}`} /></p>
+      <p onClick={toggleFilter} className="flex items-center gap-x-2 text-#F2F4FE text-opacity-75 text-3.25 md:text-3.5 cursor-pointer">{t("sort_by")} : <strong>{t(`sort_filters.${activeFilter}`)}</strong> <ArrowIcon className={`text-1.75 text-white ${isFilterOpen ? "rotate-0" : "rotate-180"}`} /></p>
       { isFilterOpen && (
         <ul className="rounded-1.25 overflow-hidden w-63.75 top-10.5 left-0 absolute flex flex-col gap-y-1px bg-#3A4374 bg-opacity-15">
           {filters.map((filter, index) => (
