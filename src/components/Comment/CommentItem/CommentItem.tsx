927ef7af364a570ada7e7ab9a93433c85941341a
Alexis Comte <alexis.comte73@icloud.com>
2024-07-16 16:54:19 +0200

    adding reply translate

--- src/components/Comment/CommentItem/CommentItem.tsx
+++ src/components/Comment/CommentItem/CommentItem.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from "react-i18next"
+
  export interface CommentItemProps {
   imageUrl: string
   fullName: string
@@ -6,6 +8,7 @@
  }
  
  export function CommentItem ({fullName, username, comment, imageUrl}: CommentItemProps) {
+  const {t} = useTranslation()
   return <div className="flex flex-col items-start gap-x-8">
     <div className="flex w-full gap-x-8">
       <img src={imageUrl} className="rounded-full h-10 w-10" />
@@ -14,7 +17,7 @@
           <h3 className="text-#3A4374 text-3.5 line-height-5 -tracking-0.19px font-bold">{fullName}</h3>
           <p className="text-#647196 text-3.5 line-height-5">{username}</p>
         </div>
-        <p className="text-3.25 text-opacity-40 font-semibold text-#4661E6">Reply (comming soon)</p>
+        <p className="text-3.25 text-opacity-40 font-semibold text-#4661E6">{t("comment.reply")} ({t("coming_soon")})</p>
       </div>
     </div>
     <p className="mt-4.25 md:ml-18 text-3.75 text-#647196 break-all">{comment}</p>
