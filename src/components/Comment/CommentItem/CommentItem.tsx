054bc8fff7a91cb196a1756215971c2e1bc0bc2e
Alexis Comte <alexis.comte73@icloud.com>
2024-07-16 13:18:32 +0200

    adding comment on reply in comment component

--- src/components/Comment/CommentItem/CommentItem.tsx
+++ src/components/Comment/CommentItem/CommentItem.tsx
@@ -14,7 +14,7 @@
           <h3 className="text-#3A4374 text-3.5 line-height-5 -tracking-0.19px font-bold">{fullName}</h3>
           <p className="text-#647196 text-3.5 line-height-5">{username}</p>
         </div>
-        <p className="text-3.25 font-semibold text-#4661E6">Reply</p>
+        <p className="text-3.25 text-opacity-40 font-semibold text-#4661E6">Reply (comming soon)</p>
       </div>
     </div>
     <p className="mt-4.25 md:ml-18 text-3.75 text-#647196 break-all">{comment}</p>
