f48b85bf8d43224350866a10ee97be249c0b1a89
Alexis Comte <alexis.comte73@icloud.com>
2024-07-16 15:13:40 +0200

    add language for add suggestion form

--- src/components/form/AddFeedbackForm/AddFeedbackForm.tsx
+++ src/components/form/AddFeedbackForm/AddFeedbackForm.tsx
@@ -8,6 +8,7 @@ import { Textarea } from "../textarea/Textarea";
 import { useState } from "react";
 import { AddFeedbackUsecaseParams } from "@/store/feedbacks/usecases/add-feedback.usecase";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 export interface AddFeedbackFormProps {
   onPostFeedback: (params: AddFeedbackUsecaseParams) => Promise<void>
@@ -15,6 +16,7 @@ export interface AddFeedbackFormProps {
 }
 
 export function AddFeedbackForm ({onPostFeedback, fieldErrors}: AddFeedbackFormProps) {
+  const {t} = useTranslation()
   const categoryOptions = Object.values(FeedbackCategory)
   const [currentOption, setCurrentOption] = useState<FeedbackCategory>(categoryOptions[0])
   const [isProcessing, setIsProcessing] = useState<boolean>(false)
@@ -40,25 +42,25 @@ export function AddFeedbackForm ({onPostFeedback, fieldErrors}: AddFeedbackFormP
   return (
     <form onSubmit={handleSubmit} className="bg-white p-6 pt-11 w-full rounded-2.5 flex flex-col gap-y-6 relative">
       <div className="bg-custom-radial h-10 w-10 md:w-14 md:h-14 rounded-full flex items-center justify-center text-white absolute -top-5 md:-top-7 text-5 md:text-6 font-bold">+</div>
-      <p className="text-4.5 font-bold text-#3A4374 line-height-6.5 mb-6">Create New Feedback</p>
+      <p className="text-4.5 font-bold text-#3A4374 line-height-6.5 mb-6">{t("add_suggestion_form.title")}</p>
       <FormGroup>
-        <InputHeader htmlFor="title" label="Feedback Title" description="Add a short, descriptive headline" />
+        <InputHeader htmlFor="title" label={t("add_suggestion_form.title_label")} description={t("add_suggestion_form.title_description")} />
         <Input name="title" errors={fieldErrors?.title} />
       </FormGroup>
       <FormGroup>
-        <InputHeader htmlFor="category" label="Category" description="Choose a category for your feedback" />
+        <InputHeader htmlFor="category" label={t("add_suggestion_form.category_label")} description={t("add_suggestion_form.category_description")} />
         <Dropdown onSelect={handleSelectOption} current={currentOption} options={categoryOptions} />
       </FormGroup>
       <FormGroup>
-        <InputHeader htmlFor="description" label="Feedback Detail" description="Include any specific comments on what should be improved, added, etc." />
+        <InputHeader htmlFor="description" label={t("add_suggestion_form.detail_label")} description={t("add_suggestion_form.detail_description")} />
         <Textarea errors={fieldErrors?.description} name="description" />
       </FormGroup>
       <div className="mt-10 flex flex-col gap-y-4 md:flex-row md:justify-end md:gap-x-4">
         <button disabled={isProcessing} type="submit" className="w-full md:w-36 md:order-last">
-          <Button isLoading={isProcessing} fullWidth>Add Feedback</Button>
+          <Button isLoading={isProcessing} fullWidth>{t("add_suggestion_form.add_button")}</Button>
         </button>
         <Link to={"/feedbacks"} className="w-full md:w-23.25">
-          <Button fullWidth type="tertiary">Cancel</Button>
+          <Button fullWidth type="tertiary">{t("add_suggestion_form.cancel_button")}</Button>
         </Link>
       </div>
     </form>
--- src/components/ui/BoBackButton/GoBackButton.tsx
+++ src/components/ui/BoBackButton/GoBackButton.tsx
@@ -1,13 +1,15 @@
 import { ArrowIcon } from "@/assets/icons";
+import { useTranslation } from "react-i18next";
 
 export type GoBackButtonStyle = "default" | "alt"
 
 export function GoBackButton({style = "default", fullWidth}: {style?: GoBackButtonStyle, fullWidth?: boolean}) {
+  const {t} = useTranslation()
 
   return (
     <div className={`flex items-center justify-center gap-x-4 cursor-pointer hover:underline rounded-2.5 ${fullWidth ? "w-full" : ""} ${style === "default" ? "hover:underline-#647196" : "bg-#373F68 hover:underline-white"}`}>
       <ArrowIcon className={`rotate-270 text-2 -mt-0.5 ${style === "default" ? "text-#4661E6" : "text-#CDD2EE"}`} />
-      <span className={`font-bold text-3.5 ${style === "default" ? "text-#647196" : "text-white"}`}>Go Back</span>
+      <span className={`font-bold text-3.5 ${style === "default" ? "text-#647196" : "text-white"}`}>{t("go_back_button_label")}</span>
     </div>
   )
 }
