dd5765316e990a56aa2afe44952474378ce522a3
Alexis Comte <alexis.comte73@icloud.com>
2024-07-19 13:15:29 +0200

    fix key for tag filter

--- src/components/TagFilter/TagFilter.tsx
+++ src/components/TagFilter/TagFilter.tsx
@@ -7,7 +7,7 @@ export function TagFilter ({ activeFilter, setActiveFilter }: {activeFilter: Tag
     <ul id="filter" className="flex flex-wrap pt-6 pl-6 pr-4.5 pb-9 bg-white rounded-2.5 md:w-55.75 h-44.5 xl:w-63.75 xl:h-41.5 gap-y-3.5 gap-x-2">
       <li className="cursor-pointer" onClick={() => setActiveFilter("all")}><Tag title={"all"} active={activeFilter === "all"} /></li>
       {Object.values(FeedbackCategory).map(c => (
-        <li className="cursor-pointer" onClick={() => setActiveFilter(c)}><Tag title={c} active={activeFilter === c} /></li>
+        <li key={c} className="cursor-pointer" onClick={() => setActiveFilter(c)}><Tag title={c} active={activeFilter === c} /></li>
       ))}
     </ul>
   )
